1c623c630701bc5d8f01a736f49c48f2b2d5d5fd
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-07-20 15:20:43 +0300

    Add pages routes

--- script.js
+++ script.js
@@ -4,12 +4,34 @@ var app = angular.module("agency",['ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/main', {
-		templateUrl: 'main.html',
+		templateUrl: 'pages/main.html',
 		controller: 'MainCtrl'
+    })
+    .when('/about', {
+		templateUrl: 'pages/about.html',
+		controller: 'MainCtrl'
+    })
+    .when('/contact', {
+		templateUrl: 'pages/contact.html',
+		controller: 'ContactCtrl'
+    })
+    .when('/services', {
+		templateUrl: 'pages/services.html',
+		controller: 'ServicesCtrl'
     })
     .otherwise({redirectTo:'/main'});
 }]);    
 
 app.controller('MainCtrl', ['$scope',function($scope){
-    console.log($scope);
+    //console.log($scope);
+    console.log('hello');
+}]);
+
+
+app.controller('ServicesCtrl', ['$scope',function($scope){
+    
+}]);
+
+app.controller('ContactCtrl', ['$scope',function($scope){
+    
 }]);
